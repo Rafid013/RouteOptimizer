b707983e88032c3fc7b5d2f160bd9c06563e8cf0
tzpranto <md.toufikzaman@gmail.com>
2017-06-02 12:14:07 +0600

    Primary Skeleton  (1405015)
    Things done:
    1. RoadInfo hold start and end point of a road.
    2.Fetched JSON data about the road is parsed by data parsed.
    3.Road is drawn on the map.
    
    Things to be done:
    1. Color the roads according to their traffic severity.
    2.Add a search box so that users can see traffic jam condition of specific place.

---
+++ RouteOptimizer/app/src/main/java/com/rrgps/routeoptimizer/RoadInfo.java
@@ -0,0 +1,38 @@
+package com.rrgps.routeoptimizer;
+
+/**
+ * Created by Toufik on 6/2/2017.
+ */
+
+import com.google.android.gms.maps.model.LatLng;
+
+
+public class RoadInfo {
+    LatLng start;
+    LatLng end;
+    int weight;
+    RoadInfo(double lat1, double lng1, double lat2, double lng2,int weight)
+    {
+        start = new LatLng(lat1,lng1);
+        end = new LatLng(lat2,lng2);
+        this.weight = weight;
+    }
+    RoadInfo(LatLng start,LatLng end,int weight)
+    {
+        this.start = start;
+        this.end=end;
+        this.weight=weight;
+    }
+    LatLng getStart()
+    {
+        return start;
+    }
+    LatLng getEnd()
+    {
+        return end;
+    }
+    int getWeight()
+    {
+        return weight;
+    }
+}
